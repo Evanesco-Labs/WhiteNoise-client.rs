ff7526e4b552e6f56d6a600e854f216c1ab74d23
CaleHH <caleihhmy@gmail.com>
2021-06-10 14:06:58 +0800

    whitenoise client

---
+++ src/client.rs
@@ -0,0 +1,112 @@
+use bytes::BufMut;
+use libp2p::{
+    Multiaddr, PeerId,
+};
+
+use whitenoisers::network::{connection::CircuitConn, node::Node};
+use futures::{StreamExt, channel::mpsc};
+
+use whitenoisers::sdk::{host, host::RunMode};
+use async_trait::async_trait;
+use log::{info, debug};
+use whitenoisers::account::account::Account;
+use crate::Client;
+
+pub async fn process_new_stream(mut node: Node) {
+    loop {
+        let mut stream = node.wait_for_relay_stream().await;
+        debug!("have new stream");
+        async_std::task::spawn(whitenoisers::network::relay_event_handler::relay_event_handler(stream.clone(), node.clone(), None));
+    }
+}
+
+pub async fn process_new_session(mut node: Node, sender: futures::channel::mpsc::UnboundedSender<String>, exist_session: std::sync::Arc<std::sync::Mutex<std::collections::HashMap<String, bool>>>) {
+    loop {
+        let len = node.circuit_map.read().unwrap().len();
+        if len > 0 {
+            let mut session_id_opt = None;
+            let mut circuit_conn = None;
+            node.circuit_map.read().unwrap().iter().for_each(|x| {
+                if !exist_session.lock().unwrap().contains_key(x.0) {
+                    session_id_opt = Some(x.0.clone());
+                    circuit_conn = Some(x.1.clone());
+                }
+            });
+            if session_id_opt.is_some() {
+                let session_id = session_id_opt.unwrap();
+                let cc = circuit_conn.unwrap();
+                if cc.transport_state.is_some() {
+                    sender.unbounded_send(session_id.clone());
+                    exist_session.lock().unwrap().insert(session_id, true);
+                }
+            }
+        }
+        async_std::task::sleep(std::time::Duration::from_millis(50)).await;
+    };
+}
+
+
+pub struct WhiteNoiseClient {
+    pub node: Node,
+    bootstrap_addr_str: String,
+    bootstrap_peer_id: PeerId,
+    new_connected_session: std::sync::Arc<futures::lock::Mutex<futures::channel::mpsc::UnboundedReceiver<String>>>,
+    exist_session: std::sync::Arc<std::sync::Mutex<std::collections::HashMap<String, bool>>>,
+}
+
+impl WhiteNoiseClient {
+    pub fn init(bootstrap_addr_str: String, key_type: whitenoisers::account::key_types::KeyType, keypair: Option<libp2p::identity::Keypair>) -> Self {
+        let mut node = host::start(None, Some(bootstrap_addr_str.clone()), RunMode::Client, keypair, key_type);
+        let parts: Vec<&str> = bootstrap_addr_str.split('/').collect();
+        let bootstrap_peer_id_str = parts.get(parts.len() - 1).unwrap();
+        let bootstrap_peer_id = PeerId::from_bytes(bs58::decode(bootstrap_peer_id_str).into_vec().unwrap().as_slice()).unwrap();
+
+        let (new_connected_sender, new_connected_receiver) = futures::channel::mpsc::unbounded();
+        let exist_session = std::sync::Arc::new(std::sync::Mutex::new(std::collections::HashMap::new()));
+        async_std::task::spawn(process_new_stream(node.clone()));
+        async_std::task::spawn(process_new_session(node.clone(), new_connected_sender, exist_session.clone()));
+        return WhiteNoiseClient {
+            node: node,
+            bootstrap_addr_str: bootstrap_addr_str,
+            bootstrap_peer_id: bootstrap_peer_id,
+            new_connected_session: std::sync::Arc::new(futures::lock::Mutex::new(new_connected_receiver)),
+            exist_session: exist_session.clone(),
+        };
+    }
+}
+
+#[async_trait]
+impl Client for WhiteNoiseClient {
+    async fn get_main_net_peers(&mut self, _cnt: i32) -> Vec<PeerId> {
+        let bootstrap_addr: Multiaddr = self.bootstrap_addr_str.parse().unwrap();
+        let peer_list = self.node.get_main_nets(10, self.bootstrap_peer_id, bootstrap_addr).await;
+        let mut peer_id_vec = Vec::with_capacity(peer_list.peers.len());
+        peer_list.peers.iter().for_each(|x| {
+            peer_id_vec.push(PeerId::from_bytes(bs58::decode(x.id.as_str()).into_vec().unwrap().as_slice()).unwrap());
+        });
+        return peer_id_vec;
+    }
+    async fn register(&mut self, peer_id: PeerId) -> bool {
+        return self.node.register_proxy(peer_id).await;
+    }
+    async fn dial(&mut self, remote_id: String) -> String {
+        return self.node.dial(remote_id).await;
+    }
+    fn get_circuit(&self, session_id: &str) -> Option<CircuitConn> {
+        self.node.circuit_map.read().unwrap().get(session_id).and_then(|x| {
+            Some(x.clone())
+        })
+    }
+    async fn send_message(&self, session_id: &str, data: &[u8]) {
+        self.node.send_message(session_id, data).await;
+    }
+    async fn disconnect_circuit(&mut self, session_id: String) {
+        self.node.handle_close_session(&session_id).await;
+    }
+    fn get_whitenoise_id(&self) -> String {
+        Account::from_keypair_to_whitenoise_id(&self.node.keypair)
+    }
+    async fn notify_next_session(&mut self) -> Option<String> {
+        self.new_connected_session.lock().await.next().await
+    }
+}
---
+++ src/node.rs
@@ -0,0 +1,152 @@
+use libp2p::{Multiaddr,
+             PeerId, Swarm, Transport,
+             core::{upgrade::{self}},
+             identify, identity, mplex, noise::{NoiseConfig, X25519Spec, Keypair},
+             request_response::{ProtocolSupport, RequestResponseConfig, RequestResponse},
+             swarm::{SwarmBuilder}, tcp::TcpConfig};
+use whitenoisers::network::{protocols::cmd_protocol::{CmdCodec, CmdProtocol}, whitenoise_behaviour::{WhitenoiseServerBehaviour, WhitenoiseClientBehaviour}};
+
+use std::{iter};
+
+use futures::channel::mpsc;
+use log::{info, debug, error};
+use whitenoisers::network::{whitenoise_behaviour::{WhitenoiseBehaviour}};
+use whitenoisers::network::protocols::proxy_protocol::{ProxyCodec, ProxyProtocol};
+use whitenoisers::network::protocols::ack_protocol::{AckCodec, AckProtocol};
+use whitenoisers::network::protocols::relay_behaviour;
+
+use whitenoisers::account::account::Account;
+
+use whitenoisers::network::whitenoise_behaviour::{self};
+
+use libp2p::kad::{
+    Kademlia,
+    KademliaConfig,
+};
+use libp2p::kad::record::store::MemoryStore;
+
+use libp2p::gossipsub::MessageId;
+use libp2p::gossipsub::{
+    GossipsubMessage, IdentTopic as Topic, MessageAuthenticity, ValidationMode,
+};
+use libp2p::{gossipsub};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+use whitenoisers::network::node::Node;
+use whitenoisers::network::proxy_event_handler::process_proxy_request;
+use whitenoisers::network::cmd_event_handler::process_cmd_request;
+
+pub fn new_client_node(bootstrap_addr_option: std::option::Option<String>, key_pair: Option<libp2p::identity::Keypair>, key_type: whitenoisers::account::key_types::KeyType) -> Node {
+    let (node_request_sender, node_request_receiver) = mpsc::unbounded();
+
+    let id_keys = match key_pair {
+        None => Account::get_default_account_keypair("./db", key_type),
+        Some(x) => x
+    };
+
+    let peer_id = id_keys.public().into_peer_id();
+    debug!("local peer id: {:?}", peer_id);
+
+    let noise_keys = Keypair::<X25519Spec>::new().into_authentic(&id_keys).unwrap();
+    let trans = TcpConfig::new()
+        .nodelay(true)
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(noise_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::default())
+        .timeout(std::time::Duration::from_millis(150))
+        .boxed();
+
+
+    let proxy_protocols = iter::once((ProxyProtocol(), ProtocolSupport::Full));
+    let proxy_cfg = RequestResponseConfig::default();
+    let proxy_behaviour = RequestResponse::new(ProxyCodec(), proxy_protocols, proxy_cfg);
+
+    let cmd_protocols = iter::once((CmdProtocol(), ProtocolSupport::Full));
+    let cmd_cfg = RequestResponseConfig::default();
+    let cmd_behaviour = RequestResponse::new(CmdCodec(), cmd_protocols, cmd_cfg);
+
+    let ack_protocols = iter::once((AckProtocol(), ProtocolSupport::Full));
+    let ack_cfg = RequestResponseConfig::default();
+    let ack_behaviour = RequestResponse::new(AckCodec(), ack_protocols, ack_cfg);
+
+    let identify_behaviour = identify::Identify::new(identify::IdentifyConfig::new(String::from("/ipfs/id/1.0.0"), id_keys.public()).with_initial_delay(std::time::Duration::from_millis(500)).with_interval(std::time::Duration::from_secs(5 * 60)));
+
+    let relay_behaviour = relay_behaviour::Relay {
+        out_events: std::collections::VecDeque::new(),
+        addresses: std::collections::HashMap::new(),
+        dive_events: std::collections::VecDeque::new(),
+    };
+
+    let event_bus = std::sync::Arc::new(std::sync::RwLock::new(std::collections::HashMap::new()));
+
+    let relay_in_streams = std::sync::Arc::new(std::sync::RwLock::new(std::collections::VecDeque::new()));
+    let relay_out_streams = std::sync::Arc::new(std::sync::RwLock::new(std::collections::VecDeque::new()));
+
+    let mut dht_cfg = KademliaConfig::default();
+    let whitnoise_dht_protocol: &[u8] = b"/whitenoise_dht/kad/1.0.0";
+    dht_cfg.set_protocol_name(std::borrow::Cow::Borrowed(whitnoise_dht_protocol));
+    dht_cfg.set_query_timeout(std::time::Duration::from_secs(5 * 60));
+    let store = MemoryStore::new(peer_id.clone());
+    let mut kad_behaviour = Kademlia::with_config(peer_id.clone(), store, dht_cfg);
+    match bootstrap_addr_option {
+        None => {}
+        Some(bootstrap_addr) => {
+            let parts: Vec<&str> = bootstrap_addr.split('/').collect();
+            let bootstrap_peer_id_str = parts.get(parts.len() - 1).unwrap();
+            let bootstrap_peer_id = PeerId::from_bytes(bs58::decode(bootstrap_peer_id_str).into_vec().unwrap().as_slice()).unwrap();
+            let mut bootstrap_addr_multiaddr: Multiaddr = bootstrap_addr.parse().unwrap();
+            let index_opt = bootstrap_addr.find("p2p");
+            if index_opt.is_some() {
+                let index = index_opt.unwrap();
+                let bootstrap_addr_parts = bootstrap_addr.split_at(index - 1);
+                bootstrap_addr_multiaddr = bootstrap_addr_parts.0.parse().unwrap();
+            }
+            kad_behaviour.add_address(&bootstrap_peer_id, bootstrap_addr_multiaddr);
+        }
+    }
+
+    let (proxy_request_sender, proxy_request_receiver) = mpsc::unbounded();
+    let (cmd_request_sender, cmd_request_receiver) = mpsc::unbounded();
+    let node = Node {
+        node_request_sender: node_request_sender.clone(),
+        event_bus: event_bus.clone(),
+        keypair: id_keys.clone(),
+        proxy_id: None,
+        relay_in_streams: relay_in_streams.clone(),
+        relay_out_streams: relay_out_streams.clone(),
+        circuit_task: std::sync::Arc::new(std::sync::RwLock::new(std::collections::HashMap::new())),
+        client_peer_map: std::sync::Arc::new(std::sync::RwLock::new(std::collections::HashMap::new())),
+        client_wn_map: std::sync::Arc::new(std::sync::RwLock::new(std::collections::HashMap::new())),
+        session_map: std::sync::Arc::new(std::sync::RwLock::new(std::collections::HashMap::new())),
+        circuit_map: std::sync::Arc::new(std::sync::RwLock::new(std::collections::HashMap::new())),
+        probe_map: std::sync::Arc::new(std::sync::RwLock::new(std::collections::HashMap::new())),
+    };
+
+    let whitenoise_behaviour = WhitenoiseBehaviour {
+        proxy_behaviour: proxy_behaviour,
+        cmd_behaviour: cmd_behaviour,
+        ack_behaviour: ack_behaviour,
+        event_bus: event_bus,
+        relay_in_streams: relay_in_streams,
+        relay_out_streams: relay_out_streams,
+        relay_behaviour: relay_behaviour,
+        proxy_request_channel: proxy_request_sender,
+        cmd_request_channel: cmd_request_sender,
+    };
+    let whitenoise_client_behaviour = WhitenoiseClientBehaviour {
+        whitenoise_behaviour: whitenoise_behaviour,
+        identify_behaviour: identify_behaviour,
+    };
+    let mut swarm1 = SwarmBuilder::new(trans, whitenoise_client_behaviour, peer_id)
+        .executor(Box::new(|fut| {
+            async_std::task::spawn(fut);
+        }))
+        .build();
+
+    async_std::task::spawn(whitenoise_behaviour::whitenoise_client_event_loop(swarm1, node_request_receiver));
+
+    async_std::task::spawn(process_proxy_request(proxy_request_receiver, node.clone()));
+    async_std::task::spawn(process_cmd_request(cmd_request_receiver, node.clone()));
+    return node;
+}
